1d0df0bf36469a0154d445c5044c0120eee379af
Richard-NG <richard.chihong.ng@gmail.com>
2019-03-04 19:12:31 +0800

    instagram photo mosaic layout

--- lib/explore/photo_grid.dart
+++ lib/explore/photo_grid.dart
@@ -1,23 +1,66 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 class PhotoGrid extends StatelessWidget {
-  List<Widget> _buildGridTiles() {
-    final List<Widget> tiles = [];
+  Widget _buildBox(double unit) {
+    return Container(
+      decoration: BoxDecoration(border: Border.all(color: Colors.red)),
+      height: unit,
+      width: unit,
+    );
+  }
 
-    for (var i = 0; i < 10; i++) {
-      tiles.add(Image.network('https://via.placeholder.com/150'));
-    }
+  Widget _build3Small(double unit) {
+    return Row(
+      children: List.generate(3, (int index) {
+        return _buildBox(unit);
+      }),
+    );
+  }
 
-    return tiles;
+  Widget _build2Small1Large(double unit) {
+    return Row(
+      children: <Widget>[
+        Column(
+          children: <Widget>[
+            _buildBox(unit),
+            _buildBox(unit),
+          ],
+        ),
+        _buildBox(unit * 2),
+      ],
+    );
+  }
+
+  Widget _build1Large2Small(double unit) {
+    return Row(
+      children: <Widget>[
+        _buildBox(unit * 2),
+        Column(
+          children: <Widget>[
+            _buildBox(unit),
+            _buildBox(unit),
+          ],
+        ),
+      ],
+    );
+  }
+
+  _buildChildren(double unit) {
+    return <Widget>[
+      _build3Small(unit),
+      _build1Large2Small(unit),
+      _build2Small1Large(unit),
+    ];
   }
 
   @override
   Widget build(BuildContext context) {
-    return GridView.count(
-      crossAxisCount: 3,
-      children: _buildGridTiles(),
-      mainAxisSpacing: 2.5,
-      crossAxisSpacing: 2.5,
+    final windowWidth = MediaQuery.of(context).size.width;
+    final unit = windowWidth / 3;
+
+    return ListView(
+      children: _buildChildren(unit),
     );
   }
 }
